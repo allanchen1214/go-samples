bf7a754197de09118dea6eb7cbf4d57268c7eba6
allanchen <allanchen@tencent.com>
2022-04-14 11:31:28 +0800

    none

--- commonuse/strlen/main.go
+++ commonuse/strlen/main.go
@@ -6,12 +6,12 @@ import (
 
 func main() {
 	str := "aancADBBNN123"
-	fmt.Printf("len(%s): %d\n", str, len(str))
+	fmt.Printf("len(%s): %d\n", str, len(str)) // 13
 
 	str = "helloworld你好世界"
-	fmt.Printf("len(%s): %d\n", str, len(str))
+	fmt.Printf("len(%s): %d\n", str, len(str)) // 22
 
 	data := []byte(str)
-	fmt.Printf("len data: %d\n", len(data))
+	fmt.Printf("len data: %d\n", len(data)) // 22
 
 }
